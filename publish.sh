2d4c4f6a7335cc091e9ac4a45e4b7e56b9fd76c8
Glen Campbell <glen@broadpool.com>
2015-11-05 13:03:05 -0800

    increased page timeout

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ FILE=mailman.css
 
 if [ -f $FILE ]; then
     aws s3 cp mailman.css s3://files.glencampbell.co \
-        --cache-control "max-age=60"
+        --cache-control "max-age=600"
 else
     echo "You're not in the right place. No $FILE here."
     exit
